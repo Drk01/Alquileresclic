bf2cdee2edc01466130b7742d775cf9cc3a7581e
Omar Monrroy <ogcm.kael@hotmail.com>
2020-04-28 16:54:14 -0500

    Creando modelo Ad con su migración

---
+++ database/migrations/2020_04_28_214022_create_ads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('cost');
+            $table->enum('currency', ['USD', 'CO']);
+            $table->enum('period', ['Hora', 'Día', 'Semanal', 'Mensual']);
+            $table->longText('description');
+            $table->string('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}

1ad3af19a8b86be1f36ea9386607cb0b572c2d6d
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-04 13:38:08 -0500

    Agregando default al campo role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->string('lastname');
             $table->string('mothersLastname');
-            $table->integer('role');
+            $table->integer('role')->default(0);
             $table->boolean('confianza');
             $table->timestamps();
         });

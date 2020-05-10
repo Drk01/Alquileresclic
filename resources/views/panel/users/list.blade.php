e43fd2781add94b538f34c4371872aeeb567e15a
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-09 19:18:20 -0500

    Lógica del botón de bloqueo de usuario

--- resources/views/panel/users/list.blade.php
+++ resources/views/panel/users/list.blade.php
@@ -1,13 +1,5 @@
-@extends('panel.layouts.panel')
-
-@section('title')
-Listado de usuarios
-@endsection
-
-@section('subtitle')
-Listado de usuarios
-@endsection
-
+@extends('panel.layouts.panel') @section('title') Listado de usuarios
+@endsection @section('subtitle') Listado de usuarios @endsection
 @section('content')
 <table id="example1" class="table table-bordered table-striped">
     <thead>
@@ -19,23 +11,29 @@ Listado de usuarios
     <tbody>
         @foreach ($users as $user)
         <tr>
-            <td>{{ $user->name }} {{ $user->lastname }} {{ $user->mothersLastname }}</td>
+            <td>
+                {{ $user->name }} {{ $user->lastname }}
+                {{ $user->mothersLastname }}
+            </td>
             <td>
                 @if ($user->block)
-                <button class="btn btn-danger btn-sm" id="block">Desbloquear</button>
+                <button class="btn btn-danger btn-sm" onclick="block({{ $user->id }})" id="block{{ $user->id }}">
+                    Desbloquear
+                </button>
                 @else
-                <button class="btn btn-success btn-sm" id="block">Bloquear</button>
+                <button class="btn btn-success btn-sm" onclick="block({{ $user->id }})" id="block{{ $user->id }}">
+                    Bloquear
+                </button>
                 @endif
 
-                @switch($user->role)
-                @case(0)
+                @switch($user->role) @case(0)
                 <button class="btn btn-primary btn-sm">Hacer supervisor</button>
-                @break
-                @case(2)
+                @break @case(2)
                 <button class="btn btn-primary btn-sm">Hacer usuario</button>
-                @break
-                @default
-                <button class="btn btn-primary btn-sm">Este usuario es Administrador</button>
+                @break @default
+                <button class="btn btn-primary btn-sm">
+                    Este usuario es Administrador
+                </button>
                 @endswitch
             </td>
         </tr>
@@ -45,8 +43,32 @@ Listado de usuarios
 <script>
     window.onload = () => {
         $("#example1").DataTable();
-    }
 
-
+        block = function (id){
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8000/api/blockuser/"+id,
+                success: function(response) {
+                    if ($("#block"+id).hasClass("btn-danger")) {
+                        $("#block"+id)
+                            .removeClass("btn-danger")
+                            .addClass("btn-success")
+                            .html("Bloquear");
+                    } else {
+                        $("#block"+id)
+                            .removeClass("btn-success")
+                            .addClass("btn-danger")
+                            .html("Desbloquear");
+                    }
+                },
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                        "content"
+                    ),
+                    "_token": $('meta[name="authApiToken"]').attr("content")
+                }
+            });
+        }
+    };
 </script>
 @endsection

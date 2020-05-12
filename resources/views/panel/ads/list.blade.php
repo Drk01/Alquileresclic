a318d232a8b6c3c91851a025e3c5250be4da4abc
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-11 22:33:10 -0500

    Creando vista para mostrar los anuncios a moderar

---
+++ resources/views/panel/ads/list.blade.php
@@ -0,0 +1,13 @@
+@extends('panel.layouts.panel')
+
+@section('title')
+Moderar anuncios
+@endsection
+
+@section('subtitle')
+Moderación de anuncios
+@endsection
+
+@section('content')
+<h1>Hola mundo</h1>
+@endsection

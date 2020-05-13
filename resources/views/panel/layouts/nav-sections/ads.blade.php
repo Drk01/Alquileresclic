fb894b2746b71cc2736f964ee26ad0d3cce59d4f
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-12 19:57:35 -0500

    Haciendo que la sección de anucios sea desplegable

--- resources/views/panel/layouts/nav-sections/ads.blade.php
+++ resources/views/panel/layouts/nav-sections/ads.blade.php
@@ -1,10 +1,22 @@
-@if (auth()->user()->role == 1 || auth()->user()->role == 2)
-<li class="nav-item">
-    <a href="{{ route('ads') }}" class="nav-link">
+<li class="nav-item has-treeview menu-open">
+    <a href="#" class="nav-link">
         <i class="nav-icon fas fa-ad"></i>
         <p>
             Anuncios
+            <i class="right fas fa-angle-left"></i>
         </p>
     </a>
+    <ul class="nav nav-treeview">
+        @if (auth()->user()->role == 1 || auth()->user()->role == 2)
+        <li class="nav-item">
+            <a href="{{ route('ads') }}" @if (\Request::route()->getName()== 'ads') class=" nav-link active"
+                @else
+                class="nav-link"
+                @endif>
+                <i class="far fa-circle nav-icon"></i>
+                <p>Moderación de anuncios</p>
+            </a>
+        </li>
+        @endif
+    </ul>
 </li>
-@endif

5f5a72d3b90f405b7b9c5aa69a4a828f77c4cb38
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-19 01:19:35 -0500

    Plantilla para crear anuncios

---
+++ resources/views/panel/ads/create.blade.php
@@ -0,0 +1,13 @@
+@extends('panel.layouts.panel')
+
+@section('title')
+Crear anuncio
+@endsection
+
+@section('subtitle')
+¡Cree su anuncio!
+@endsection
+
+@section('content')
+<h2>Hola mundo</h2>
+@endsection

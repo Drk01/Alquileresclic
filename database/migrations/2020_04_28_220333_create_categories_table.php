d130e3e03dfd82766c7f73504c4684b43eb3c3dd
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-21 16:20:10 -0500

    Agregando campo default al campo parent_id

--- database/migrations/2020_04_28_220333_create_categories_table.php
+++ database/migrations/2020_04_28_220333_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('parent_id');
+            $table->string('parent_id')->default('No');
             $table->timestamps();
         });
     }

cdc01f0f22725520b1ed4d274f014c0bf5cb0449
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-22 16:41:37 -0500

    Salvando cambios al perfil

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Profile;
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['saveProfileChanges']);
     }
 
     public function getEditProfileForm()
@@ -16,7 +18,23 @@ class ProfileController extends Controller
         return view('panel.profiles.editMyProfile');
     }
 
-    public function saveProfileChanges()
+    public function saveProfileChanges(Request $request, $id)
     {
+        $user = User::find($id)->update($request->all());
+
+        $profile = Profile::firstOrCreate(
+            ['user_id' => $id],
+            [
+                'publicName' => $request->publicName,
+                'address' => $request->address,
+                'city' => $request->city,
+                'type' => $request->type
+            ]
+        )->update([
+            'publicName' => $request->publicName,
+            'address' => $request->address,
+            'city' => $request->city,
+            'type' => $request->type
+        ]);
     }
 }

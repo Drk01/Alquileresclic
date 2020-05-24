ecf3ba77dd1c04741c215faaf1e7cc6e78775224
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-24 06:49:31 -0500

    Creando selector de categorías

--- resources/views/panel/ads/create.blade.php
+++ resources/views/panel/ads/create.blade.php
@@ -9,5 +9,48 @@ Crear anuncio
 @endsection
 
 @section('content')
-<h2>Hola mundo</h2>
+<link rel="stylesheet" href="{{ asset('css/categorySelector.css') }}">
+
+<div class="row">
+    <div class="col">
+        <h5>Seleccione la categoría de su anuncio: </h5>
+        <div>
+            <div class="mt-6" id="categorySelector">
+            </div>
+
+
+        </div>
+    </div>
+    <div class="col">
+        <ul id="secondaryCategories" class="categoryList">
+
+        </ul>
+    </div>
+</div>
+<script>
+    window.onload = function (){
+        $('#categorySelector').append('<ul id="categoryList"></ul>');
+        $.get(route('getMainCategories').url()).done((categories) => {
+            categories.forEach(category => {
+                $('#categoryList').append(`<li onclick="getSecondaries(${category.id})" class="primaryItem">${category.name} <i class="fas fa-chevron-right rightArrow"></i></li>`);
+            });
+        })
+
+        getSecondaries = function (id){
+            console.log(id)
+            $('#secondaryCategories').empty();
+            $.get(route('getSecondaryCategories',id).url()).done((subcategories) => {
+                subcategories.forEach(subcategory => {
+                    $('#secondaryCategories').append(`
+                     <li class="primaryItem" onclick="subcatSelected(${subcategory.id})">${subcategory.name}</li>
+                    `)
+                });
+            })
+        }
+
+        subcatSelected = function(id){
+            console.log(id);
+        }
+    }
+</script>
 @endsection

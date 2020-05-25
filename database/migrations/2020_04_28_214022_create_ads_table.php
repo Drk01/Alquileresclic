37547012fa5e9f218a5facdc0cd20c765c0be8cf
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-24 20:51:04 -0500

    Agregando el campo 'negociable' al modelo Ad

--- database/migrations/2020_04_28_214022_create_ads_table.php
+++ database/migrations/2020_04_28_214022_create_ads_table.php
@@ -21,6 +21,7 @@ class CreateAdsTable extends Migration
             $table->enum('period', ['Hora', 'Día', 'Semanal', 'Mensual']);
             $table->longText('description');
             $table->string('code');
+            $table->boolean('negotiable');
             $table->foreignId('profile_id');
             $table->timestamps();
         });

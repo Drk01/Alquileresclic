0eeeb26229e9e0d2139095716e4088beca1aea4c
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-26 10:30:02 -0500

    Creando e implementando dangerMessage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,9 @@
 @if (session()->has('message'))
 @include('panel.layouts.alerts.alertSuccess', ['message' => session('message')])
 @endif
+
+@if (session('dangerMessage'))
+@include('panel.layouts.alerts.alertDanger', ['message' => session('dangerMessage')])
+@endif
 <h2>Hola mundo</h2>
 @endsection

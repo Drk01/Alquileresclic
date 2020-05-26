2cd85f48a285de681bb6fc369a37b7aa95724f01
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-26 10:09:38 -0500

    Creando alerta para mensajes

---
+++ resources/views/panel/layouts/alerts/alertSuccess.blade.php
@@ -0,0 +1,7 @@
+<div class="alert alert-success alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+        &times;
+    </button>
+    <h5><i class="icon fas fa-check"></i> ¡Atención!</h5>
+    {{ session("message") }}
+</div>

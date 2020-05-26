d07369525fbccd05c60fcf4bc56f72b97faad21b
Omar Monrroy <ogcm.kael@hotmail.com>
2020-05-26 09:58:12 -0500

    Realizando proceso de envio de anuncios a moderación

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Ad;
 use App\Category;
+use App\Image;
+use App\Profile;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class AdController extends Controller
 {
@@ -30,5 +35,58 @@ class AdController extends Controller
 
     public function sendToModeration(Request $request)
     {
+        //Validando que llegaron todos los datos:
+        $validador = $request->validate([
+            'title' => ['required'],
+            'description' => ['required'],
+            'category' => ['required'],
+            'cost' => ['required'],
+            'currency' => ['required'],
+            'period' => ['required'],
+            'negotiable' => ['required'],
+            'images' => ['required']
+        ]);
+
+        //Obteniendo el usuario que crea el anuncio.
+        $usuario = User::find(auth()->user()->id);
+
+        try {
+            //Creando el anuncio:
+            $anuncio = new Ad;
+            $anuncio->title = $request->title;
+            $anuncio->description = $request->description;
+            $anuncio->cost = $request->cost;
+            $anuncio->currency = $request->currency;
+            $anuncio->period = $request->period;
+
+            if ($request->negotiable === "Si") {
+                $anuncio->negotiable = true;
+            } else {
+                $anuncio->negotiable = false;
+            }
+
+            $anuncio->code = Str::random(6);
+            $anuncio->profile_id = $usuario->profile()->first()->id;
+            $anuncio->save();
+
+            // Salvar las imágenes del anuncio.
+
+            foreach ($request->images as $image) {
+                $path =  $image->store('ads');
+                $image = new Image([
+                    'image_link' => $path
+                ]);
+                $anuncio->images()->save($image);
+            }
+
+            //Restar token de anuncio.
+            $usuario = Profile::where('user_id', auth()->user()->id)->first();
+            $usuario->remaining_ads = $usuario->remaining_ads - 1;
+            $usuario->save();
+
+            return redirect(route('home'))->with('message',  'Su anuncio ha sido enviado a moderación');
+        } catch (\Throwable $th) {
+            return response($th);
+        }
     }
 }

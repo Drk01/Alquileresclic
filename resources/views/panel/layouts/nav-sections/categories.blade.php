a347577a1de545ceb28507de0bac02c8b20325bb
Omar Monrroy <ogcm.kael@hotmail>
2020-05-28 01:13:19 -0500

    Activando la clase active en las páginas activas

--- resources/views/panel/layouts/nav-sections/categories.blade.php
+++ resources/views/panel/layouts/nav-sections/categories.blade.php
@@ -1,5 +1,5 @@
 @if (auth()->user()->role == 1)
-<li class="nav-item has-treeview menu-close">
+<li class="nav-item has-treeview menu-open">
     <a href="#" class="nav-link">
         <i class="nav-icon fas fa-tag"></i>
         <p>
@@ -9,13 +9,17 @@
     </a>
     <ul class="nav nav-treeview">
         <li class="nav-item">
-            <a href="{{ route('createCategory') }}" class="nav-link">
+            <a href="{{ route('createCategory') }}" @if (\Request::route()->getName()== 'createCategory') class=" nav-link active"
+                @else
+                class="nav-link" @endif>
                 <i class="far fa-circle nav-icon"></i>
                 <p>Crear categoría</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ route('categoriesList') }}" class="nav-link">
+            <a href="{{ route('categoriesList') }}" @if (\Request::route()->getName()== 'categoriesList') class=" nav-link active"
+                @else
+                class="nav-link" @endif>
                 <i class="far fa-circle nav-icon"></i>
                 <p>Listado de categorías</p>
             </a>
--- resources/views/panel/layouts/nav-sections/perfíl.blade.php
+++ resources/views/panel/layouts/nav-sections/perfíl.blade.php
@@ -1,4 +1,4 @@
-<li class="nav-item has-treeview menu-close">
+<li class="nav-item has-treeview menu-open">
     <a href="#" class="nav-link">
         <i class="nav-icon fas fa-user"></i>
         <p>
@@ -8,13 +8,19 @@
     </a>
     <ul class="nav nav-treeview">
         <li class="nav-item">
-            <a href="{{ route('editMyProfile') }}" class="nav-link">
+            <a href="{{ route('editMyProfile') }}" @if (\Request::route()->getName()== 'editMyProfile') class=" nav-link active"
+                @else
+                class="nav-link"
+                @endif>
                 <i class="far fa-user nav-icon"></i>
                 <p>Editar mi perfil</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ route('changePassword') }}" class="nav-link">
+            <a href="{{ route('changePassword') }}" @if (\Request::route()->getName()== 'changePassword') class=" nav-link active"
+                @else
+                class="nav-link"
+                @endif>
                 <i class="fas fa-key nav-icon"></i>
                 <p>Cambiar contraseña</p>
             </a>

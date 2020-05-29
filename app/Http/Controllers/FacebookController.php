f3cdfffde81b1b5788534c41f954c2690e32af8a
Omar Monrroy <ogcm.kael@hotmail>
2020-05-28 20:39:38 -0500

    Lógica de login con Facebook

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
 
 class FacebookController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    public function redirectToProvider()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('facebook')->user();
+
+        $user = User::firstOrCreate(
+            ['email' => $user->getEmail()],
+            [
+                'name' => $user->getName(),
+                'lastname' => $user->getName(),
+                'mothersLastname' => $user->getName(),
+                'password' => bcrypt(Str::random(20))
+            ]
+        );
+
+        Auth::login($user, true);
+
+        return redirect(route('home'));
+    }
 }

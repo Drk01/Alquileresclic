d3d886df445632b47f2fac1f39aeb2979bece640
Omar Monrroy <ogcm.kael@hotmail>
2020-05-29 17:30:36 -0500

    Creando método para mostrar la vista Packages

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PackageController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function showPackageOptions()
+    {
+        return view('panel.packages.packages');
+    }
 }

2f776c3712987e35afc557bf77c49713a80cb5ed
Omar Monrroy <ogcm.kael@hotmail>
2020-05-29 17:16:03 -0500

    Creando plantilla para sección de anuncios favoritos

---
+++ resources/views/panel/ads/showMyFavorites.blade.php
@@ -0,0 +1,13 @@
+@extends('panel.layouts.panel')
+
+@section('title')
+Mis favoritos
+@endsection
+
+@section('subtitle')
+Mis anuncios favoritos
+@endsection
+
+@section('content')
+<h2>Hola mundo</h2>
+@endsection

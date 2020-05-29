898d752f441f32c1ab669636db79f781f1fe5f53
Omar Monrroy <ogcm.kael@hotmail>
2020-05-28 20:59:42 -0500

    Creando vista para ver mis anuncios

---
+++ resources/views/panel/ads/myAds.blade.php
@@ -0,0 +1,14 @@
+@extends('panel.layouts.panel')
+
+@section('title')
+Mis anuncios
+@endsection
+
+@section('subtitle')
+Mis anuncios
+@endsection
+
+@section('content')
+<h2>Mis anuncios</h2>
+
+@endsection

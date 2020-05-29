362f4d19d0c164f15224cbf1afb0484451dfb1a9
Omar Monrroy <ogcm.kael@hotmail>
2020-05-29 10:06:32 -0500

    Creando relación 1:M User-Favorite

--- app/User.php
+++ app/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->morphMany(Image::class, 'imageable');
     }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
 }

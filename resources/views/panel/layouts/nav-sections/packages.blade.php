c17b449e67a96209536e0a48bf641aa996302517
Omar Monrroy <ogcm.kael@hotmail>
2020-05-29 22:01:18 -0500

    Creando sección de paquetes en el navbar

---
+++ resources/views/panel/layouts/nav-sections/packages.blade.php
@@ -0,0 +1,8 @@
+<li class="nav-item">
+    <a href="{{ route('showPackageOptions') }}" class="nav-link">
+        <i class="nav-icon fas fa-shopping-cart"></i>
+        <p>
+            Paquetes
+        </p>
+    </a>
+</li>

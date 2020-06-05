ebddb9aa2370b3492965a5afd151b1c86443f484
Omar Monrroy <ogcm.kael@hotmail>
2020-06-05 14:14:19 -0500

    Obteniendo los últimos 6 posts aprobados

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -10,7 +10,7 @@ class FrontEndController extends Controller
 {
     public function getIndex()
     {
-        $ultimosAnuncios = Ad::orderBy('created_at', 'DESC')->take(6)->get();
+        $ultimosAnuncios = Ad::where('status', 'Aprobado')->orderBy('created_at', 'DESC')->take(6)->get();
 
         return view('welcome', [
             'ultimosAnuncios' => $ultimosAnuncios

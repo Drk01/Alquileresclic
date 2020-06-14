11a62a518479a00aa61447e3b565fdb0392ff04b
Omar Monrroy <ogcm.kael@hotmail>
2020-06-14 05:21:59 -0500

    Agregando indicative a fillable

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'lastnames', 'mothersLastname', 'phone'
+        'name', 'email', 'password', 'lastnames', 'mothersLastname', 'phone', 'indicative'
     ];
 
     /**

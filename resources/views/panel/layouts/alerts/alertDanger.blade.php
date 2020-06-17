8bd3d37dbb7fc3b29ec869ac6e4299a8cddda525
Omar Monrroy <ogcm.kael@hotmail.com>
2020-06-17 04:01:20 -0500

    Agregando enlace para editar el perfíl

--- resources/views/panel/layouts/alerts/alertDanger.blade.php
+++ resources/views/panel/layouts/alerts/alertDanger.blade.php
@@ -1,3 +1,12 @@
+@if ($message == "Por favor cree su perfíl antes de continuar")
+<div class="alert alert-danger alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+        &times;
+    </button>
+    <h5><i class="icon fas fa-ban"></i> ¡Atención!</h5>
+    Por favor <a href="{{ route('editMyProfile') }}">cree su perfíl</a> antes de continuar
+</div>
+@else
 <div class="alert alert-danger alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
         &times;
@@ -5,3 +14,4 @@
     <h5><i class="icon fas fa-ban"></i> ¡Atención!</h5>
     {{ $message }}
 </div>
+@endif
